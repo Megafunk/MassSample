350d91610af81cc9ad2887810e123d4dbe0b6d26
Megafunk <megafunk9@gmail.com>
2022-03-18 16:59:17 -0600

    added new trait for adding MassActor fragments
    renamed dataassets to meet the allar standard (prefix based on parent class instead of DA)

---
+++ Source/MassSample/Traits/ActorMassTrait.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ActorMassTrait.h"
+
+#include "MassActorSubsystem.h"
+#include "MassCommonFragments.h"
+#include "MassEntityTemplateRegistry.h"
+#include "Fragments/MassSampleFragments.h"
+
+void UActorMassTrait::BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const
+{
+	FMassActorFragment& MassActorFragment = BuildContext.AddFragment_GetRef<FMassActorFragment>();
+	
+	//@TODO: This one is really pushing it! man...
+	MassActorFragment.SetNoHandleMapUpdate(FMassEntityHandle(), Actor, bIsOwnedByMass);
+	
+	
+}
+
+UActorMassInitializer::UActorMassInitializer()
+{
+	ObservedType = FMassActorFragment::StaticStruct();
+	Operation = EMassObservedOperation::Add;
+}
+
+void UActorMassInitializer::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FMassActorFragment>(EMassFragmentAccess::ReadWrite);
+
+}
+
+void UActorMassInitializer::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+
+	EntityQuery.ForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+
+				const TArrayView<FMassActorFragment> ActorsList = Context.GetMutableFragmentView<FMassActorFragment>();
+
+				for (int32 i = 0; i < Context.GetNumEntities(); ++i)
+				{
+					const FMassEntityHandle Entity = Context.GetEntity(i);
+
+					FMassActorFragment& ActorFragment = ActorsList[i];
+
+					if(IsValid(ActorFragment.GetMutable()))
+					{
+						ActorFragment.SetAndUpdateHandleMap(Entity,ActorFragment.GetMutable(),ActorFragment.IsOwnedByMass());
+
+					}
+
+					
+				}
+				
+		
+	});
+	
+}
---
+++ Source/MassSample/Traits/ActorMassTrait.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassEntityTraitBase.h"
+#include "MassObserverProcessor.h"
+#include "ActorMassTrait.generated.h"
+
+/**
+ * 
+ */
+
+UCLASS(meta = (DisplayName = "Sample Actor Trait"))
+class MASSSAMPLE_API UActorMassTrait : public UMassEntityTraitBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const override;
+	
+	UPROPERTY(EditAnywhere)
+	AActor* Actor;
+	UPROPERTY(EditAnywhere)
+	bool bIsOwnedByMass;
+	
+};
+
+UCLASS()
+class MASSSAMPLE_API UActorMassInitializer : public UMassObserverProcessor
+{
+	GENERATED_BODY()
+protected:
+
+	UActorMassInitializer();
+	
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	FMassEntityQuery EntityQuery;
+};
