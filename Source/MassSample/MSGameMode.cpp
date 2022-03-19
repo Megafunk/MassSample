5ddb23a0204b3b4cbddc97fc0971e38b2bf34eb9
Megafunk <megafunk9@gmail.com>
2022-03-19 03:00:24 -0600

    [FIX] hopefully last of the Blueprint mixup fixes

--- Source/MassSample/MSGameMode.cpp
+++ Source/MassSample/MSGameMode.cpp
@@ -6,10 +6,5 @@
 
 AMSGameMode::AMSGameMode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
