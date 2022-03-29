18f48646a51fc9b0f88c6c81b3841147c18a8847
Megafunk <megafunk9@gmail.com>
2022-03-28 21:50:35 -0600

    benchmark stuff

---
+++ Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MSPathologicalBenchmarkProcessor.h"
+
+// void UMSPathologicalBenchmarkProcessor::Combinations(TArray<UScriptStruct*> array, TArray<UScriptStruct*> result,int32 length )
+// {
+// 	if (length==0)
+// 	{
+//
+// 		FString output;
+// 		for (auto ScriptStruct : result)
+// 		{
+// 			output+=ScriptStruct->GetName();
+// 		}
+// 		UE_LOG( LogTemp, Error, TEXT("MSPathologicalBenchmarkProcessor: %s"),*output);
+// 		return;
+// 	}
+// 	
+// 	for (int i = 0; i <= array.Num();++i)
+// 	{
+// 		result[result.Num()-length] = array[i];
+// 		Combinations(array, array, length-1);
+// 	}
+// }
+
+
+
+
+void UMSPathologicalBenchmarkProcessor::Initialize(UObject& Owner)
+{
+	// TArray<UScriptStruct*> Provinces = {
+	// 	FAlberta::StaticStruct(),
+	// 	FBritishColumbia::StaticStruct(),
+	// 	FManitoba::StaticStruct(),
+	// 	FNewBrunswick::StaticStruct(),
+	// 	FNewfoundlandandLabrador::StaticStruct(),
+	// 	FNovaScotia::StaticStruct(),
+	// 	FOntario::StaticStruct(),
+	// 	FPrinceEdwardIsland::StaticStruct(),
+	// 	FQuebec::StaticStruct(),
+	// 	FSaskatchewan::StaticStruct()};
+	//
+	//
+	// TArray<FMassArchetypeHandle> Archetypes;
+	
+	//Combinations(Provinces,TArray<UScriptStruct*>{},10);
+
+}
+
+
+
+// void UMSPathologicalBenchmarkProcessor::ConfigureQueries()
+// {
+//
+// }
+//
+// void UMSPathologicalBenchmarkProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+// {
+// }
---
+++ Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.h
@@ -0,0 +1,92 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassProcessor.h"
+#include "MSPathologicalBenchmarkProcessor.generated.h"
+
+
+
+// for example you have 10 components, and each component has a 50% chance of being added to the entity
+
+
+USTRUCT()
+struct MASSSAMPLE_API FAlberta : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+USTRUCT()
+struct MASSSAMPLE_API FBritishColumbia : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+USTRUCT()
+struct MASSSAMPLE_API FManitoba : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+USTRUCT()
+struct MASSSAMPLE_API FNewBrunswick : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+USTRUCT()
+struct MASSSAMPLE_API FNewfoundlandandLabrador : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+
+
+USTRUCT()
+struct MASSSAMPLE_API FNovaScotia : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct MASSSAMPLE_API FOntario : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct MASSSAMPLE_API FPrinceEdwardIsland : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};USTRUCT()
+struct MASSSAMPLE_API FQuebec : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+USTRUCT()
+struct MASSSAMPLE_API FSaskatchewan : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+
+
+// UCLASS()
+// class MASSSAMPLE_API UMSPathologicalBenchmarkProcessor : public UMassProcessor
+// {
+// 	GENERATED_BODY()
+// public:
+// 	virtual void Initialize(UObject& Owner) override;
+// 	//static void Combinations(TArray<UScriptStruct*> array, TArray<UScriptStruct*> result,int32 length );
+// protected:
+// 	virtual void ConfigureQueries() override;
+// 	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+//
+// };
