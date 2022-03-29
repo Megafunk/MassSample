c1ae81729d752adeb68e25f05a3554f682e6534e
Megafunk <megafunk9@gmail.com>
2022-03-29 01:59:57 -0600

    added new "patholigic" benchmark that pushes random archetype creation to the extreme!
    credit to vblanco20-1 for the idea

--- Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.cpp
+++ Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.cpp
@@ -3,58 +3,174 @@
 
 #include "MSPathologicalBenchmarkProcessor.h"
 
-// void UMSPathologicalBenchmarkProcessor::Combinations(TArray<UScriptStruct*> array, TArray<UScriptStruct*> result,int32 length )
-// {
-// 	if (length==0)
-// 	{
-//
-// 		FString output;
-// 		for (auto ScriptStruct : result)
-// 		{
-// 			output+=ScriptStruct->GetName();
-// 		}
-// 		UE_LOG( LogTemp, Error, TEXT("MSPathologicalBenchmarkProcessor: %s"),*output);
-// 		return;
-// 	}
-// 	
-// 	for (int i = 0; i <= array.Num();++i)
-// 	{
-// 		result[result.Num()-length] = array[i];
-// 		Combinations(array, array, length-1);
-// 	}
-// }
+//This is just to double check my math works out, we don't need it at all.
+ void UMSPathologicalBenchmarkProcessor::CombinationsRecursive(UMassEntitySubsystem* EntitySubsystem, int32 length,int32 offset = 0 )
+ {
+ 	if (length ==0)
+ 	{
+ 		FString output;
+ 		for (const auto ScriptStruct : Combinations)
+ 		{
+ 			if(ScriptStruct !=nullptr) output+=ScriptStruct->GetName();
+ 		}
+
+ 		++NumCombinations;
+ 		
+ 		//UE_LOG( LogTemp, Warning, TEXT("MSPathologicalBenchmarkProcessor: %s,%i"),*output,Combinations.Num());
+ 		return;
+ 	}
+ 	for(int i = offset; i<=Provinces.Num() - length; ++i)
+ 	{
+ 		Combinations.Add(Provinces[i]);
+ 		CombinationsRecursive(EntitySubsystem,length-1,i+1);
+ 		Combinations.RemoveAt(Combinations.Num()-1);
+			
+ 	}
+ }
 
 
 
 
 void UMSPathologicalBenchmarkProcessor::Initialize(UObject& Owner)
 {
-	// TArray<UScriptStruct*> Provinces = {
-	// 	FAlberta::StaticStruct(),
-	// 	FBritishColumbia::StaticStruct(),
-	// 	FManitoba::StaticStruct(),
-	// 	FNewBrunswick::StaticStruct(),
-	// 	FNewfoundlandandLabrador::StaticStruct(),
-	// 	FNovaScotia::StaticStruct(),
-	// 	FOntario::StaticStruct(),
-	// 	FPrinceEdwardIsland::StaticStruct(),
-	// 	FQuebec::StaticStruct(),
-	// 	FSaskatchewan::StaticStruct()};
-	//
-	//
-	// TArray<FMassArchetypeHandle> Archetypes;
-	
-	//Combinations(Provinces,TArray<UScriptStruct*>{},10);
+	UMassEntitySubsystem* EntitySubsystem = GetWorld()->GetSubsystem<UMassEntitySubsystem>();
 
+ 	//10 Different fragments!
+ 	//TODO: let us turn this amount up and down?
+	 Provinces = {
+	 	FAlberta::StaticStruct(),
+	 	FBritishColumbia::StaticStruct(),
+	 	FManitoba::StaticStruct(),
+	 	FNewBrunswick::StaticStruct(),
+	 	FNewfoundlandandLabrador::StaticStruct(),
+	 	FNovaScotia::StaticStruct(),
+	 	FOntario::StaticStruct(),
+	 	FPrinceEdwardIsland::StaticStruct(),
+	 	FQuebec::StaticStruct(),
+	 	FSaskatchewan::StaticStruct()};
+	
+
+
+	for(int i = 1; i<=10; ++i)
+	{
+		Combinations.Empty();
+		CombinationsRecursive(EntitySubsystem,i);
+	}
+ 	
+ 	
+
+ 	//UE_LOG( LogTemp, Warning, TEXT("MSPathologicalBenchmarkProcessor done! Archetypes possible: %i"),Archetypes.Num());
+
+
+
+	//create 1000 entities for now
+ 	int v = 0;
+
+ 	for(int i = 0; i<100; ++i)
+ 	{
+ 		for (auto Fragment : Provinces)
+ 		{
+ 			//For now, we don't actually add the province fragment to the entity
+ 			EntitySubsystem->CreateEntity(EntitySubsystem->CreateArchetype({FPathologicFragment::StaticStruct()}));
+ 			++v;
+ 		}
+ 	}
+ 	
+ 	UE_LOG( LogTemp, Warning, TEXT("MSPathologicalBenchmarkProcessor done! Entities created: %i"),v);
+ 	
 }
 
 
 
-// void UMSPathologicalBenchmarkProcessor::ConfigureQueries()
-// {
-//
-// }
-//
-// void UMSPathologicalBenchmarkProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
-// {
-// }
+ void UMSPathologicalBenchmarkProcessor::ConfigureQueries()
+ {
+	//quick and dirty way to disable this for now if you want
+ 	if constexpr (false)
+ 	{
+ 		//ideally this would be a tag but that appears to break the query?
+ 		PathologicQuery.AddRequirement<FPathologicFragment>(EMassFragmentAccess::ReadWrite);
+ 		// PathologicQuery.AddRequirement(FAlberta::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FBritishColumbia::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FManitoba::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FNewBrunswick::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FNewfoundlandandLabrador::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FNovaScotia::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FOntario::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FPrinceEdwardIsland::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FQuebec::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 		// PathologicQuery.AddRequirement(FSaskatchewan::StaticStruct(),EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+ 	}
+
+ 	
+	 //FIXME: this triggers validity errors on the query internally?
+	 // for (auto Fragment : Provinces)
+	 // {
+		//  PathologicQuery.AddRequirement(Fragment,EMassFragmentAccess::ReadWrite,EMassFragmentPresence::Optional);
+	 // }
+ }
+
+ void UMSPathologicalBenchmarkProcessor::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+ {
+
+ 	QUICK_SCOPE_CYCLE_COUNTER(STAT_MASS_PathologicalBenchmark);
+
+ 	PathologicQuery.ForEachEntityChunk(EntitySubsystem,Context,
+		 [&,this](FMassExecutionContext& Context)
+		 {
+		 	
+		 	const int32 QueryLength = Context.GetNumEntities();
+
+		 	for (int32 i = 0; i < QueryLength; ++i)
+		 	{
+
+		 		auto entity = Context.GetEntity(i);
+
+		 		//I cannot think of a better way to do this due to them being templates!
+		 		//Macro time?
+		 		if(FMath::RandBool())
+		 		{
+		 			Context.Defer().AddFragment<FAlberta>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FBritishColumbia>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FManitoba>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FNewBrunswick>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FNewfoundlandandLabrador>(entity);
+		 		}	
+		 		if(FMath::RandBool())
+		 		{
+		 			Context.Defer().AddFragment<FNovaScotia>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FOntario>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FPrinceEdwardIsland>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FQuebec>(entity);
+		 		}
+		 		if(FMath::RandBool())
+		 		{
+					 Context.Defer().AddFragment<FSaskatchewan>(entity);
+		 		}
+		 	}
+		 });
+
+ 	
+
+ 	
+ }
--- Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.h
+++ Source/MassSample/Experimental/Benchmark/MSPathologicalBenchmarkProcessor.h
@@ -6,87 +6,107 @@
 #include "MassProcessor.h"
 #include "MSPathologicalBenchmarkProcessor.generated.h"
 
-
-
-// for example you have 10 components, and each component has a 50% chance of being added to the entity
-
-
+/*
+ *
+ *This is an ECS benchmark called Pathological invented by vblanco
+ *It creates entities with a random sets of components as a stress test.
+ *We start with 10 different components and create combinations of each from length 1 to 10.
+ *Then we creates a massive list of 1023 different archetypes
+ *This one of the worst performance cases for archetype style ECS implementations
+ */
 USTRUCT()
-struct MASSSAMPLE_API FAlberta : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};
-USTRUCT()
-struct MASSSAMPLE_API FBritishColumbia : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};
-USTRUCT()
-struct MASSSAMPLE_API FManitoba : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};
-USTRUCT()
-struct MASSSAMPLE_API FNewBrunswick : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};
-USTRUCT()
-struct MASSSAMPLE_API FNewfoundlandandLabrador : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};
-
-
-
-USTRUCT()
-struct MASSSAMPLE_API FNovaScotia : public FMassFragment
+struct FAlberta : public FMassFragment
 {
 	GENERATED_BODY()
 	int64 stuff;
 };
 
 USTRUCT()
-struct MASSSAMPLE_API FOntario : public FMassFragment
+struct FBritishColumbia : public FMassFragment
 {
 	GENERATED_BODY()
 	int64 stuff;
 };
 
 USTRUCT()
-struct MASSSAMPLE_API FPrinceEdwardIsland : public FMassFragment
-{
-	GENERATED_BODY()
-	int64 stuff;
-};USTRUCT()
-struct MASSSAMPLE_API FQuebec : public FMassFragment
+struct FManitoba : public FMassFragment
 {
 	GENERATED_BODY()
 	int64 stuff;
 };
+
 USTRUCT()
-struct MASSSAMPLE_API FSaskatchewan : public FMassFragment
+struct FNewBrunswick : public FMassFragment
 {
 	GENERATED_BODY()
 	int64 stuff;
 };
 
+USTRUCT()
+struct FNewfoundlandandLabrador : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
 
+USTRUCT()
+struct FNovaScotia : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
 
-// UCLASS()
-// class MASSSAMPLE_API UMSPathologicalBenchmarkProcessor : public UMassProcessor
-// {
-// 	GENERATED_BODY()
-// public:
-// 	virtual void Initialize(UObject& Owner) override;
-// 	//static void Combinations(TArray<UScriptStruct*> array, TArray<UScriptStruct*> result,int32 length );
-// protected:
-// 	virtual void ConfigureQueries() override;
-// 	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
-//
-// };
+USTRUCT()
+struct FOntario : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct FPrinceEdwardIsland : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct FQuebec : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct FSaskatchewan : public FMassFragment
+{
+	GENERATED_BODY()
+	int64 stuff;
+};
+
+USTRUCT()
+struct FPathologicFragment : public FMassFragment
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class MASSSAMPLE_API UMSPathologicalBenchmarkProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+public:
+	void CombinationsRecursive(UMassEntitySubsystem* EntitySubsystem, int32 length, int32 offset);
+	virtual void Initialize(UObject& Owner) override;
+	//static void Combinations(TArray<UScriptStruct*> array, TArray<UScriptStruct*> result,int32 length );
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+
+	TArray<UScriptStruct*> Combinations;
+	TArray<UScriptStruct*> Provinces;
+
+	int NumCombinations;
+
+	FMassEntityQuery PathologicQuery;
+	
+};
