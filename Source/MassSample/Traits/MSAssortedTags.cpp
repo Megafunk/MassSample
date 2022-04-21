4466298a5fc37d9f4bace28b4fe0258c2fd255b0
Megafunk <megafunk9@gmail.com>
2022-04-20 21:28:57 -0600

    Added assorted tags trait

---
+++ Source/MassSample/Traits/MSAssortedTags.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MSAssortedTags.h"
+
+#include "MassEntityTemplateRegistry.h"
+
+void UMSAssortedTags::BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, UWorld& World) const
+{
+	for (auto& Tag : Tags)
+	{
+		const UScriptStruct* TagStruct = Tag.GetScriptStruct();
+
+		if(TagStruct->IsChildOf(FMassTag::StaticStruct()))
+		{
+			BuildContext.AddTag(*TagStruct);
+		}
+	}
+}
