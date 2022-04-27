7e2e4ab088d149704203594917312a4eed33b95c
Megafunk <megafunk9@gmail.com>
2022-04-26 22:19:20 -0600

    added parallelization command to engine ini BP support work (I will save it for later like I said I would...)

--- Source/MassSample/Common/Misc/MSBPFunctionLibarary.h
+++ Source/MassSample/Common/Misc/MSBPFunctionLibarary.h
@@ -58,5 +58,79 @@ class MASSSAMPLE_API UMSBPFunctionLibarary : public UBlueprintFunctionLibrary
 
 	UFUNCTION(BlueprintCallable, Category = "Mass", meta = (WorldContext = "WorldContextObject"))
 	static void AddFragmentToEntity(FFragmentWrapper Fragment , FEntityHandleWrapper Entity ,const UObject* WorldContextObject);
-	
+
+
+
+
+
+
+
+
+
+	//thanks to https://forums.unrealengine.com/t/tutorial-how-to-accept-wildcard-structs-in-your-ufunctions/18968/11?u=megafunk
+
+
+	UFUNCTION(
+		BlueprintCallable, 
+		Category = "Mass",
+		CustomThunk,
+		meta = (
+			Keywords = "Arbitrary Struct",
+			CustomStructureParam = "AnyStruct"
+	))
+	static void ReceiveSomeStruct(UStruct* AnyStruct);
+
+	DECLARE_FUNCTION(execReceiveSomeStruct)
+	{
+		// Steps into the stack, walking to the next property in it
+		Stack.Step(Stack.Object, NULL);
+
+		// Grab the last property found when we walked the stack
+		// This does not contains the property value, only its type information
+		FStructProperty* StructProperty = CastField<FStructProperty>(Stack.MostRecentProperty);
+
+		
+
+		// Grab the base address where the struct actually stores its data
+		// This is where the property value is truly stored
+		void* StructPtr = Stack.MostRecentPropertyAddress;
+
+		// We need this to wrap up the stack
+		P_FINISH;
+
+		ReceiveSomeStruct_impl(StructProperty, StructPtr);
+	}
+
+	/*
+	* Example function for iterating through all properties of a struct
+	* @param StructProperty    The struct property reflection data
+	* @param StructPtr        The pointer to the struct value
+	*/
+	static void ReceiveSomeStruct_impl(FStructProperty* Property, void* StructPtr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Property->GetClass()->GetName());
+		//check for null
+		if (Property)
+		{
+
+
+				UE_LOG(LogTemp, Warning, TEXT("%s"), *Property->Struct->GetClass()->GetName());
+
+				
+			
+		}
+
+
+
+
+
+
+
+
+
+
+			
+
+		
+	}
 };
