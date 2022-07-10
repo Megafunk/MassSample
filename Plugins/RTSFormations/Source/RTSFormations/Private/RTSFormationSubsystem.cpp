208140345af06a9f1019c26ff9a5d36dd68659f0
Ji-Rath <garethaguilar@gmail.com>
2022-07-10 15:34:44 -0500

    Add RTSFormationSubsystem for handling entity destruction and unit array

---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RTSFormationSubsystem.h"
+
+#include "MassAgentComponent.h"
+#include "MassEntitySubsystem.h"
+#include "MassSignalSubsystem.h"
+#include "RTSFormationProcessors.h"
+
+void URTSFormationSubsystem::DestroyEntity(UMassAgentComponent* Entity)
+{
+	UMassEntitySubsystem* EntitySubsystem = GetWorld()->GetSubsystem<UMassEntitySubsystem>();
+	EntitySubsystem->Defer().DestroyEntity(Entity->GetEntityHandle());
+	UE_LOG(LogTemp, Error, TEXT("Entity: %d"), Entity->GetEntityHandle().Index)
+	Units.Remove(Entity->GetEntityHandle());
+	GetWorld()->GetSubsystem<UMassSignalSubsystem>()->SignalEntities(FormationUpdated, Units);
+}
---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Common/Misc/MSBPFunctionLibrary.h"
+#include "RTSFormationSubsystem.generated.h"
+
+class UMassAgentComponent;
+struct FMassEntityHandle;
+/**
+ * 
+ */
+UCLASS()
+class RTSFORMATIONS_API URTSFormationSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	// Stores the num of units in the formation
+	UPROPERTY()
+	TArray<FMassEntityHandle> Units;
+
+	UFUNCTION(BlueprintCallable)
+	void DestroyEntity(UMassAgentComponent* Entity);
+};
