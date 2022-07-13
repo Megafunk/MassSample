4e7704acacb8e1ca2051e15826ebd26e79756b26
Ji-Rath <garethaguilar@gmail.com>
2022-07-13 18:25:51 -0500

    Implement multiple units Add Spawning units function

--- Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.cpp
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.cpp
@@ -6,7 +6,10 @@
 #include "MassAgentComponent.h"
 #include "MassEntitySubsystem.h"
 #include "MassSignalSubsystem.h"
+#include "MassSpawnerSubsystem.h"
+#include "RTSAgentTraits.h"
 #include "RTSFormationProcessors.h"
+#include "Kismet/GameplayStatics.h"
 
 void URTSFormationSubsystem::DestroyEntity(UMassAgentComponent* Entity)
 {
@@ -14,14 +17,62 @@ void URTSFormationSubsystem::DestroyEntity(UMassAgentComponent* Entity)
 	check(EntitySubsystem);
 	
 	EntitySubsystem->Defer().DestroyEntity(Entity->GetEntityHandle());
-	
-	// My current observer implementation doesnt handle entity destruction properly, so the logic is performed here for the time
-	//Units.Remove(Entity->GetEntityHandle());
-	//GetWorld()->GetSubsystem<UMassSignalSubsystem>()->SignalEntities(FormationUpdated, Units);
 }
 
-void URTSFormationSubsystem::SetUnitPosition(FVector NewPosition)
+void URTSFormationSubsystem::SetUnitPosition(const FVector& NewPosition, int UnitIndex)
 {
-	UnitPosition = NewPosition;
-	GetWorld()->GetSubsystem<UMassSignalSubsystem>()->SignalEntities(FormationUpdated, Units);
+	if (!ensure(Units.IsValidIndex(UnitIndex))) { return; }
+
+	DrawDebugPoint(GetWorld(), Units[UnitIndex].UnitPosition, 20.f, FColor::Red, false, 10.f);
+	DrawDebugPoint(GetWorld(), NewPosition, 20.f, FColor::Green, false, 10.f);
+	
+	Units[UnitIndex].Angle = FMath::RadiansToDegrees(acosf(FVector::DotProduct(FVector::ForwardVector, (NewPosition - Units[UnitIndex].UnitPosition).GetSafeNormal())));
+	//UE_LOG(LogTemp, Error, TEXT("Angle: %f"), Units[UnitIndex].Angle);
+	Units[UnitIndex].ForwardDirection = (NewPosition-Units[UnitIndex].UnitPosition).GetSafeNormal();
+	Units[UnitIndex].UnitPosition = NewPosition;
+	if (Units[UnitIndex].Entities.Num() > 0)
+		GetWorld()->GetSubsystem<UMassSignalSubsystem>()->SignalEntities(FormationUpdated, Units[UnitIndex].Entities);
+}
+
+void URTSFormationSubsystem::SpawnEntitiesForUnit(int UnitIndex, const UMassEntityConfigAsset* EntityConfig, int Count)
+{
+	if (!ensure(Units.IsValidIndex(UnitIndex))) { return; }
+
+	UMassEntitySubsystem* EntitySubsystem = GetWorld()->GetSubsystem<UMassEntitySubsystem>();
+
+	// Reserve space for the new units, the space will be filled in a processor
+	// Give a random Unit position for the heck of it
+	Units[UnitIndex].Entities.Reserve(Units[UnitIndex].Entities.Num()+Count);
+	Units[UnitIndex].UnitPosition = FVector(FMath::RandRange(-1000.f,1000.f), FMath::RandRange(-1000.f,1000.f), 0.f);
+	
+	TArray<FMassEntityHandle> Entities;
+	const FMassEntityTemplate* EntityTemplate = EntityConfig->GetConfig().GetOrCreateEntityTemplate(*UGameplayStatics::GetPlayerPawn(this, 0), *EntityConfig);
+
+	// We are doing a little bit of work here since we are setting the unit index manually
+	// Otherwise, using SpawnEntities would be perfectly fine
+	// @todo find if there is a better way to modify templates in code
+	TArray<FMassEntityHandle> SpawnedEntities;
+	TSharedRef<UMassEntitySubsystem::FEntityCreationContext> CreationContext = EntitySubsystem->BatchCreateEntities(EntityTemplate->GetArchetype(), Count, SpawnedEntities);
+
+	// Set the template default values for the entities
+	TConstArrayView<FInstancedStruct> FragmentInstances = EntityTemplate->GetInitialFragmentValues();
+	EntitySubsystem->BatchSetEntityFragmentsValues(CreationContext->GetChunkCollection(), FragmentInstances);
+
+	// Set unit index for entities
+	FRTSFormationAgent FormationAgent;
+	FormationAgent.UnitIndex = UnitIndex;
+	
+	TArray<FInstancedStruct> Fragments;
+	Fragments.Add(FConstStructView::Make(FormationAgent));
+	EntitySubsystem->BatchSetEntityFragmentsValues(CreationContext->GetChunkCollection(), Fragments);
+	
+	//@todo Look into MassArchetypeSubChunks since it might be a key to iterating through units
+}
+
+int URTSFormationSubsystem::SpawnNewUnit(const UMassEntityConfigAsset* EntityConfig, int Count)
+{
+	int UnitIndex = Units.Num();
+	Units.AddDefaulted(1);
+	SpawnEntitiesForUnit(UnitIndex, EntityConfig, Count);
+	return UnitIndex;
 }
--- Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.h
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSFormationSubsystem.h
@@ -6,6 +6,46 @@
 #include "Common/Misc/MSBPFunctionLibrary.h"
 #include "RTSFormationSubsystem.generated.h"
 
+UENUM()
+enum FormationType
+{
+	Rectangle,
+	Circle
+};
+
+USTRUCT(BlueprintType)
+struct FUnitInfo
+{
+	GENERATED_BODY()
+	
+public:
+	// Entities in the unit
+	UPROPERTY()
+	TArray<FMassEntityHandle> Entities;
+
+	UPROPERTY(BlueprintReadOnly)
+	FVector UnitPosition;
+
+	UPROPERTY()
+	FVector ForwardDirection;
+
+	UPROPERTY()
+	float FormationLength = 0;
+
+	UPROPERTY()
+	TEnumAsByte<FormationType> Formation = Rectangle;
+
+	UPROPERTY()
+	float Angle = 0;
+
+	FUnitInfo() {};
+
+	FUnitInfo(const TArray<FMassEntityHandle>& Entities)
+	{
+		this->Entities.Append(Entities);
+	}
+};
+
 class UMassAgentComponent;
 struct FMassEntityHandle;
 /**
@@ -18,15 +58,18 @@ class RTSFORMATIONS_API URTSFormationSubsystem : public UWorldSubsystem
 	
 public:
 	// Stores the num of units in the formation
-	UPROPERTY()
-	TArray<FMassEntityHandle> Units;
+	UPROPERTY(BlueprintReadOnly)
+	TArray<FUnitInfo> Units;
 
 	UFUNCTION(BlueprintCallable)
 	void DestroyEntity(UMassAgentComponent* Entity);
 
 	UFUNCTION(BlueprintCallable)
-	void SetUnitPosition(FVector NewPosition);
-	
-	UPROPERTY()
-	FVector UnitPosition;
+	void SetUnitPosition(const FVector& NewPosition, int UnitIndex = 0);
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnEntitiesForUnit(int UnitIndex, const UMassEntityConfigAsset* EntityConfig, int Count);
+
+	UFUNCTION(BlueprintCallable)
+	int SpawnNewUnit(const UMassEntityConfigAsset* EntityConfig, int Count);
 };
