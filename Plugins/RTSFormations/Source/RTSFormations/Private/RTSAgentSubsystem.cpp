ec3ab602abb83e42d1b4ccc6dd779d2a073b5cdf
Ji-Rath <garethaguilar@gmail.com>
2022-09-01 21:55:33 -0500

    Add RTSAgentSubsystem for agent position tracking and communication to mass processors

---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentSubsystem.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RTSAgentSubsystem.h"
+
+#include "LaunchEntityProcessor.h"
+#include "MassCommandBuffer.h"
+#include "MassEntitySubsystem.h"
+#include "Engine/World.h"
+
+void URTSAgentSubsystem::LaunchEntities(const FVector& Location, float Radius) const
+{
+	TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("FindNearbyEntities"));
+
+	UMassEntitySubsystem* EntitySubsystem = GetWorld()->GetSubsystem<UMassEntitySubsystem>();
+	
+	// Query items in radius
+	TArray<FMassEntityHandle> Entities;
+	const FBox Bounds(Location - FVector(Radius, Radius, 0.f), Location + FVector(Radius, Radius, 0.f));
+	AgentHashGrid.Query(Bounds,Entities);
+
+	if (EntitySubsystem)
+	{
+		FLaunchEntityFragment LaunchEntityFragment;
+		LaunchEntityFragment.Origin = Location;
+		
+		for(const FMassEntityHandle& Entity : Entities)
+		{
+			EntitySubsystem->Defer().PushCommand(FCommandAddFragmentInstance(Entity,
+				FConstStructView::Make(LaunchEntityFragment)));
+		}
+	}
+}
---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentSubsystem.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "HierarchicalHashGrid2D.h"
+#include "MassEntityTypes.h"
+#include "RTSAgentSubsystem.generated.h"
+
+
+typedef THierarchicalHashGrid2D<2, 4, FMassEntityHandle> RTSAgentHashGrid2D;
+
+/**
+ * 
+ */
+UCLASS()
+class RTSFORMATIONS_API URTSAgentSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	RTSAgentHashGrid2D AgentHashGrid = RTSAgentHashGrid2D();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure = false)
+	void LaunchEntities(const FVector& Location, float Radius) const;
+};
