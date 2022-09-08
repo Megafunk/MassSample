c931510b567cae7a8ef7c848ceb211ea2c463141
Ji-Rath <garethaguilar@gmail.com>
2022-09-08 11:12:28 -0500

    Add RTSAgentProcessors to handle hash grid positions of RTS agents

---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentProcessors.cpp
@@ -0,0 +1,120 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RTSAgentProcessors.h"
+#include "MassCommonFragments.h"
+#include "MassEntitySubsystem.h"
+#include "RTSAgentTraits.h"
+#include "Engine/World.h"
+
+//----------------------------------------------------------------------//
+//  URTSUpdateHashPosition
+//----------------------------------------------------------------------//
+void URTSUpdateHashPosition::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FRTSFormationAgent>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.AddRequirement<FAgentRadiusFragment>(EMassFragmentAccess::ReadOnly);
+}
+
+void URTSUpdateHashPosition::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TConstArrayView<FTransformFragment> TransformFragments = Context.GetFragmentView<FTransformFragment>();
+		TArrayView<FRTSFormationAgent> FormationAgents = Context.GetMutableFragmentView<FRTSFormationAgent>();
+		TConstArrayView<FAgentRadiusFragment> RadiusFragments = Context.GetFragmentView<FAgentRadiusFragment>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			FRTSFormationAgent& RTSAgent = FormationAgents[EntityIndex];
+			const FVector& Location = TransformFragments[EntityIndex].GetTransform().GetLocation();
+			const float Radius = RadiusFragments[EntityIndex].Radius;
+			
+			const FBox NewBounds(Location - FVector(Radius, Radius, 0.f), Location + FVector(Radius, Radius, 0.f));
+			RTSAgent.CellLoc = AgentSubsystem->AgentHashGrid.Move(Context.GetEntity(EntityIndex), RTSAgent.CellLoc, NewBounds);
+		}
+	});
+}
+
+void URTSUpdateHashPosition::Initialize(UObject& Owner)
+{
+	AgentSubsystem = UWorld::GetSubsystem<URTSAgentSubsystem>(Owner.GetWorld());
+}
+
+//----------------------------------------------------------------------//
+//  URTSInitializeHashPosition
+//----------------------------------------------------------------------//
+URTSInitializeHashPosition::URTSInitializeHashPosition()
+{
+	ObservedType = FRTSFormationAgent::StaticStruct();
+	Operation = EMassObservedOperation::Add;
+}
+
+void URTSInitializeHashPosition::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FRTSFormationAgent>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FAgentRadiusFragment>(EMassFragmentAccess::ReadOnly);
+}
+
+void URTSInitializeHashPosition::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TConstArrayView<FTransformFragment> TransformFragments = Context.GetFragmentView<FTransformFragment>();
+		TArrayView<FRTSFormationAgent> FormationAgents = Context.GetMutableFragmentView<FRTSFormationAgent>();
+		TConstArrayView<FAgentRadiusFragment> RadiusFragments = Context.GetFragmentView<FAgentRadiusFragment>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			FRTSFormationAgent& RTSAgent = FormationAgents[EntityIndex];
+			const FVector& Location = TransformFragments[EntityIndex].GetTransform().GetLocation();
+			const float Radius = RadiusFragments[EntityIndex].Radius;
+			
+			const FBox NewBounds(Location - FVector(Radius, Radius, 0.f), Location + FVector(Radius, Radius, 0.f));
+			RTSAgent.CellLoc = AgentSubsystem->AgentHashGrid.Add(Context.GetEntity(EntityIndex), NewBounds);
+		}
+	});
+}
+
+void URTSInitializeHashPosition::Initialize(UObject& Owner)
+{
+	AgentSubsystem = UWorld::GetSubsystem<URTSAgentSubsystem>(Owner.GetWorld());
+}
+
+//----------------------------------------------------------------------//
+//  URTSRemoveHashPosition
+//----------------------------------------------------------------------//
+
+URTSRemoveHashPosition::URTSRemoveHashPosition()
+{
+	ObservedType = FRTSFormationAgent::StaticStruct();
+	Operation = EMassObservedOperation::Remove;
+}
+
+void URTSRemoveHashPosition::ConfigureQueries()
+{
+	EntityQuery.AddRequirement<FRTSFormationAgent>(EMassFragmentAccess::ReadOnly);
+}
+
+void URTSRemoveHashPosition::Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context)
+{
+	EntityQuery.ParallelForEachEntityChunk(EntitySubsystem, Context, [this](FMassExecutionContext& Context)
+	{
+		TConstArrayView<FRTSFormationAgent> FormationAgents = Context.GetFragmentView<FRTSFormationAgent>();
+		
+		for (int32 EntityIndex = 0; EntityIndex < Context.GetNumEntities(); ++EntityIndex)
+		{
+			const FRTSFormationAgent& RTSAgent = FormationAgents[EntityIndex];
+			
+			AgentSubsystem->AgentHashGrid.Remove(Context.GetEntity(EntityIndex), RTSAgent.CellLoc);
+		}
+	});
+}
+
+void URTSRemoveHashPosition::Initialize(UObject& Owner)
+{
+	Super::Initialize(Owner);
+	AgentSubsystem = UWorld::GetSubsystem<URTSAgentSubsystem>(Owner.GetWorld());
+}
---
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentProcessors.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassObserverProcessor.h"
+#include "MassProcessor.h"
+#include "RTSAgentProcessors.generated.h"
+
+// Update hash grid position of entities
+UCLASS()
+class RTSFORMATIONS_API URTSUpdateHashPosition : public UMassProcessor
+{
+	GENERATED_BODY()
+	
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+	TObjectPtr<class URTSAgentSubsystem> AgentSubsystem;
+};
+
+// Initialize the hash grid position of RTS agents
+UCLASS()
+class RTSFORMATIONS_API URTSInitializeHashPosition : public UMassObserverProcessor
+{
+	GENERATED_BODY()
+
+	URTSInitializeHashPosition();
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+	TObjectPtr<class URTSAgentSubsystem> AgentSubsystem;
+};
+
+// Remove the entities from the hash grid when destroyed/no longer an RTSAgent
+UCLASS()
+class RTSFORMATIONS_API URTSRemoveHashPosition : public UMassObserverProcessor
+{
+	GENERATED_BODY()
+
+	URTSRemoveHashPosition();
+	virtual void ConfigureQueries() override;
+	virtual void Execute(UMassEntitySubsystem& EntitySubsystem, FMassExecutionContext& Context) override;
+	virtual void Initialize(UObject& Owner) override;
+
+	FMassEntityQuery EntityQuery;
+	TObjectPtr<URTSAgentSubsystem> AgentSubsystem;
+};
