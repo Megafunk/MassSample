f9eea6618436f81c80506f9acc04080eedcb2e59
Ji-Rath <garethaguilar@gmail.com>
2022-09-08 11:16:20 -0500

    Adjust LaunchEntities function to use QuerySmall and Signal processing due to roadblock with observers - needs further investigation

--- Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentSubsystem.cpp
+++ Plugins/RTSFormations/Source/RTSFormations/Private/RTSAgentSubsystem.cpp
@@ -10,24 +10,29 @@
 
 void URTSAgentSubsystem::LaunchEntities(const FVector& Location, float Radius) const
 {
-	TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("FindNearbyEntities"));
+	TRACE_CPUPROFILER_EVENT_SCOPE(TEXT("LaunchEntities"));
 
 	UMassEntitySubsystem* EntitySubsystem = GetWorld()->GetSubsystem<UMassEntitySubsystem>();
 	
 	// Query items in radius
 	TArray<FMassEntityHandle> Entities;
 	const FBox Bounds(Location - FVector(Radius, Radius, 0.f), Location + FVector(Radius, Radius, 0.f));
-	AgentHashGrid.Query(Bounds,Entities);
+	AgentHashGrid.QuerySmall(Bounds,Entities);
 
 	if (EntitySubsystem)
 	{
 		FLaunchEntityFragment LaunchEntityFragment;
 		LaunchEntityFragment.Origin = Location;
+		LaunchEntityFragment.Magnitude = 500.f;
+
 		
 		for(const FMassEntityHandle& Entity : Entities)
 		{
 			EntitySubsystem->Defer().PushCommand(FCommandAddFragmentInstance(Entity,
 				FConstStructView::Make(LaunchEntityFragment)));
 		}
+		// hacky fix since I couldnt get observer working for the life of me
+		if (Entities.Num())
+			GetWorld()->GetSubsystem<UMassSignalSubsystem>()->DelaySignalEntities(LaunchEntity, Entities,0.1f);
 	}
 }
