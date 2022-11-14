116142595d60d1a9bf4d80d1f9b91a1817c592b9
Megafunk <megafunk9@gmail.com>
2022-11-13 23:10:04 -0700

    [NEW] Gym benchmark now shows off fast path transform scene component translation
    This is overall faster than regular scene component movement but might have issues with physics
    This can be used outside of Mass as well, just be careful

---
+++ Plugins/MassCommunitySample/Source/MassCommunitySample/Experimental/Benchmark/MSSceneCompTransformToActorTranslators.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MSSceneCompTransformToActorTranslators.h"
+
+#include "MassCommonTypes.h"
+#include "Translators/MassSceneComponentLocationTranslator.h"
+
+UMSTransformToSceneCompTranslatorFastPath::UMSTransformToSceneCompTranslatorFastPath()
+	: EntityQuery(*this)
+{
+	ExecutionFlags = (int32)EProcessorExecutionFlags::All;
+	ExecutionOrder.ExecuteInGroup = UE::Mass::ProcessorGroupNames::UpdateWorldFromMass;
+	ExecutionOrder.ExecuteAfter.Add(UE::Mass::ProcessorGroupNames::Movement);
+	
+	RequiredTags.Add<FMSMassTransformToSceneCompFastPathTag>();
+
+	bRequiresGameThreadExecution = true;
+}
+
+void UMSTransformToSceneCompTranslatorFastPath::ConfigureQueries()
+{
+	AddRequiredTagsToQuery(EntityQuery);
+	EntityQuery.AddRequirement<FMassSceneComponentWrapperFragment>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadWrite);
+	EntityQuery.RequireMutatingWorldAccess(); // due to mutating World. We can do part of this async maybe...
+}
+
+void UMSTransformToSceneCompTranslatorFastPath::Execute(FMassEntityManager& EntityManager,
+                                                                 FMassExecutionContext& Context)
+{
+	EntityQuery.ForEachEntityChunk(EntityManager, Context, [this](FMassExecutionContext& Context)
+	{
+		const TConstArrayView<FMassSceneComponentWrapperFragment> ComponentList = Context.GetFragmentView<
+			FMassSceneComponentWrapperFragment>();
+		const TArrayView<FTransformFragment> LocationList = Context.GetMutableFragmentView<FTransformFragment>();
+
+		const int32 NumEntities = Context.GetNumEntities();
+
+
+		for (int32 i = 0; i < NumEntities; ++i)
+		{
+			if (USceneComponent* RootComponent = ComponentList[i].Component.Get())
+			{
+				SetWorldTransformFastPath(RootComponent, LocationList[i].GetTransform());
+			}
+		}
+	});
+}
+
+UMSSceneCompTransformToMassTranslator::UMSSceneCompTransformToMassTranslator() : EntityQuery(*this)
+{
+	ExecutionFlags = (int32)EProcessorExecutionFlags::All;
+	ExecutionOrder.ExecuteInGroup = UE::Mass::ProcessorGroupNames::SyncWorldToMass;
+	RequiredTags.Add<FMSSceneComponentTransformToMassTag>();
+}
+
+void UMSSceneCompTransformToMassTranslator::ConfigureQueries()
+{
+	AddRequiredTagsToQuery(EntityQuery);
+	EntityQuery.AddRequirement<FMassSceneComponentWrapperFragment>(EMassFragmentAccess::ReadOnly);
+	EntityQuery.AddRequirement<FTransformFragment>(EMassFragmentAccess::ReadWrite);
+}
+
+void UMSSceneCompTransformToMassTranslator::Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context)
+{
+	EntityQuery.ForEachEntityChunk(EntityManager, Context, [this](FMassExecutionContext& Context)
+	{
+		const TConstArrayView<FMassSceneComponentWrapperFragment> ComponentList = Context.GetFragmentView<FMassSceneComponentWrapperFragment>();
+		const TArrayView<FTransformFragment> LocationList = Context.GetMutableFragmentView<FTransformFragment>();
+
+		const int32 NumEntities = Context.GetNumEntities();
+		for (int32 i = 0; i < NumEntities; ++i)
+		{
+			if (const USceneComponent* AsComponent = ComponentList[i].Component.Get())
+			{
+				LocationList[i].GetMutableTransform() = AsComponent->GetComponentTransform();
+			}
+		}
+	});
+}
---
+++ Plugins/MassCommunitySample/Source/MassCommunitySample/Experimental/Benchmark/MSSceneCompTransformToActorTranslators.h
@@ -0,0 +1,87 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassEntityTemplateRegistry.h"
+#include "MassTranslator.h"
+#include "MSSceneCompTransformToActorTranslators.generated.h"
+
+
+USTRUCT()
+struct MASSCOMMUNITYSAMPLE_API FMSMassTransformToSceneCompFastPathTag : public FMassTag
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class MASSCOMMUNITYSAMPLE_API UMSTransformToSceneCompTranslatorFastPath : public UMassTranslator
+{
+	GENERATED_BODY()
+
+public:
+	UMSTransformToSceneCompTranslatorFastPath();
+
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+
+	// Thanks to vblanco for this fast transform setting trick
+	// According to him the first two steps (to world and updatebounds) can be threaded but not the render dirty
+	FORCEINLINE static void SetWorldTransformFastPath(USceneComponent* InComp, FTransform InTransform)
+	{
+        
+		InComp->SetComponentToWorld(InTransform);
+		InComp->UpdateBounds();
+		InComp->MarkRenderTransformDirty();
+		for (auto Component : InComp->GetAttachChildren())
+		{
+			// This * transforms from local space to world space!
+			InTransform = Component->GetRelativeTransform() * InTransform;
+
+			//These are to support non-relative transforms (could probably omit as this is rare?)
+			if(InComp->IsUsingAbsoluteLocation())
+			{
+				InTransform.CopyTranslation(InTransform);
+			}
+
+			if(InComp->IsUsingAbsoluteRotation())
+			{
+				InTransform.CopyRotation(InTransform);
+			}
+
+			if(InComp->IsUsingAbsoluteScale())
+			{
+				InTransform.CopyScale3D(InTransform);
+			}
+
+			// Recursive!
+			SetWorldTransformFastPath(Component,InTransform);
+		}
+	};
+
+	FMassEntityQuery EntityQuery;
+};
+
+
+USTRUCT()
+struct MASSCOMMUNITYSAMPLE_API FMSSceneComponentTransformToMassTag : public FMassTag
+{
+	GENERATED_BODY()
+};
+
+UCLASS()
+class MASSCOMMUNITYSAMPLE_API UMSSceneCompTransformToMassTranslator : public UMassTranslator
+{
+	GENERATED_BODY()
+
+public:
+	UMSSceneCompTransformToMassTranslator();
+
+protected:
+	virtual void ConfigureQueries() override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+	FMassEntityQuery EntityQuery;
+};
