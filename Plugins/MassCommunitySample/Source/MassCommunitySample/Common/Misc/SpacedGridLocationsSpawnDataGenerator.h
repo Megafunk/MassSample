d2d391f0e75043bcdb3d5ed4b9fb19785cdc27ff
Megafunk <megafunk9@gmail.com>
2023-07-17 22:28:45 -0600

    SpacedGridLocationsSpawnDataGenerator now spaces things out in a nicer way

--- Plugins/MassCommunitySample/Source/MassCommunitySample/Common/Misc/SpacedGridLocationsSpawnDataGenerator.h
+++ Plugins/MassCommunitySample/Source/MassCommunitySample/Common/Misc/SpacedGridLocationsSpawnDataGenerator.h
@@ -16,51 +16,102 @@ class MASSCOMMUNITYSAMPLE_API USpacedGridLocationsSpawnDataGenerator : public UM
 {
 	GENERATED_BODY()
 public:
-	virtual void Generate(UObject& QueryOwner, TConstArrayView<FMassSpawnedEntityType> EntityTypes, int32 Count,FFinishedGeneratingSpawnDataSignature& FinishedGeneratingSpawnPointsDelegate) const override
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	double Spacing = 100.0f;
+
+	virtual void Generate(UObject& QueryOwner, TConstArrayView<FMassSpawnedEntityType> EntityTypes, int32 Count,
+	                      FFinishedGeneratingSpawnDataSignature& FinishedGeneratingSpawnPointsDelegate) const override
 	{
-		
 		const FTransform& OwnerTransform = CastChecked<AActor>(&QueryOwner)->GetTransform();
 
 		if (Count <= 0 || !OwnerTransform.IsValid())
 		{
 			FinishedGeneratingSpawnPointsDelegate.Execute(TArray<FMassEntitySpawnDataGeneratorResult>());
-			return;
 		}
-			
 		TArray<FMassEntitySpawnDataGeneratorResult> Results;
-			
-		for (int32 i = 0; i < EntityTypes.Num(); i++)
-		{
-			const FMassSpawnedEntityType& EntityType = EntityTypes[i];
-			const int32 EntityCount = int32(Count * EntityType.Proportion);
+		BuildResultsFromEntityTypes(Count, EntityTypes, Results);
 
-			if (EntityCount > 0)
+
+		TArray<int32> NumLeftPerResults;
+
+		
+		{
+			int32 CountPostBuildResults = 0;
+			for (FMassEntitySpawnDataGeneratorResult& Res : Results)
 			{
-				FMassEntitySpawnDataGeneratorResult& Res = Results.AddDefaulted_GetRef();
-				Res.NumEntities = EntityCount;
-				Res.EntityConfigIndex = i;
 				Res.SpawnDataProcessor = UMassSpawnLocationProcessor::StaticClass();
-				
 				Res.SpawnData.InitializeAs<FMassTransformsSpawnData>();
 				FMassTransformsSpawnData& Transforms = Res.SpawnData.GetMutable<FMassTransformsSpawnData>();
-				Transforms.Transforms.Reserve(Res.NumEntities);
+				Transforms.Transforms.SetNumUninitialized(Res.NumEntities);
+				NumLeftPerResults.Add(Res.NumEntities);
+				CountPostBuildResults += Res.NumEntities;
+			}
+			Count = CountPostBuildResults;
+		}
+		
 
-				int32 SquareWidth = FMath::Sqrt((float)EntityCount);
+		
+		int32 SquaredRows = FMath::Sqrt((float)Count);
+		
+		const FVector BaseLocation = OwnerTransform.GetLocation();
 
-				double Spacing = 100.0f;
 
-				
-				for (int32 iX = 0; iX < SquareWidth; ++iX)
-				{
-					for (int32 iY = 0; iY < SquareWidth; ++iY)
-					{
-						const FVector Point = OwnerTransform.GetLocation() + FVector(Spacing * (iX - SquareWidth), Spacing * (iY - SquareWidth), 0);
-						
-						Transforms.Transforms.Add(FTransform(OwnerTransform.GetRotation(),Point));
-					}
-				}
+
+		// Distribute points in the grid
+		TArray<int32> Grid;
+		Grid.SetNum(SquaredRows);
+		for (int32 i = 0; i < SquaredRows; i++)
+		{
+			Grid[i] = SquaredRows;
+		}
+		// Handle leftover points
+		int32 Remaining = Count - (SquaredRows * SquaredRows);
+		while (Remaining > 0)
+		{
+			if (Remaining >= SquaredRows)
+			{
+				// If there's enough points to fill a row, add a row
+				int32 NewRow = SquaredRows;
+				Grid.Add(NewRow);
+				Remaining -= SquaredRows;
+			}
+			else
+			{
+				// If there's less than a row's worth of points, add them to a new row
+				int32 NewRow = Remaining;
+				Grid.Add(NewRow);
+				Remaining = 0;
 			}
 		}
+
+
+		// Stuff them all into our results randomly
+		int32 ResultsIndex = 0;
+		for (int32 i = 0; i < Grid.Num(); i++)
+		{
+			for (int32 j = 0; j < Grid[i]; ++j)
+			{
+				FTransform NewTransform = FTransform(BaseLocation + FVector(i*Spacing,j*Spacing,0));
+				NewTransform.SetRotation(OwnerTransform.Rotator().Quaternion());
+				// scatter the transforms we make over the results instead of making more arrays
+				while(ResultsIndex <= NumLeftPerResults.Num())
+				{
+					int32 ResultNumLeft = NumLeftPerResults[ResultsIndex];
+					if(ResultNumLeft > 0)
+					{
+						FMassEntitySpawnDataGeneratorResult& Res = Results[ResultsIndex];
+						FMassTransformsSpawnData& TransformData = Res.SpawnData.GetMutable<FMassTransformsSpawnData>();
+						TransformData.Transforms[ResultNumLeft - 1] = NewTransform;
+						--NumLeftPerResults[ResultsIndex];
+						break;
+					}
+					ResultsIndex++;
+				}
+			}
+			
+		}
+		
 		FinishedGeneratingSpawnPointsDelegate.Execute(Results);
 	};
 };
